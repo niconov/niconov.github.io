ed505c80805c6446f46146fb24c5f2f19f7408a7
Artemii Nikonov <artemii.niconov@gmail.com>
2016-12-11 19:17:55 +0300

    Init for Webpack SPA

--- js/helpers.coffee
+++
@@ -1,8 +0,0 @@
-#
-# function animateAlongPath( path, element, start, dur ) {
-#     var len = Snap.path.getTotalLength( path );
-#     Snap.animate( start, len, function( value ) {
-#             var movePoint = Snap.path.getPointAtLength( path, value );
-#             element.attr({ x: movePoint.x, y: movePoint.y });
-#     }, dur);
-# };
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+webpack = require('webpack');
+path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+webpackConfig = {
+		context: __dirname,
+		entry: {
+				bundle: './app/app.js',
+				styles: './style/main.sass'
+		},
+		output: {
+				filename: '[name].js',
+				path: './build',
+				library: '[name]'
+		},
+		resolve: {
+				extensions: ['', '.js', '.jsx']
+		},
+		devtool: '#cheap-module-source-map',
+		module: {
+				loaders: [
+						{
+								test: /\.jsx?$/,
+								exclude: [/node_modules/],
+								loader: "babel-loader",
+								query: {
+										presets: ['es2015', 'react', 'stage-0', 'stage-1']
+								}
+						},
+						{
+								test: /\.sass$/,
+								loader: ExtractTextPlugin.extract('css-loader?sourceMap!autoprefixer-loader?{browsers:["last 2 version"]}!sass-loader?indentedSyntax&sourceMap&sourceMapContents')
+						},
+						{
+								test: /\.css$/,
+								loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+						},
+						{
+								test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+								loader: 'file-loader'
+						}
+				]
+		},
+		plugins: [
+				new ExtractTextPlugin('styles.css', {
+						allChunks: true
+				})
+		]
+};
+module.exports = webpackConfig;
